b575d98ffff4cd021c2c2151b2578fe98f2fd055
leeyebin <leeyebin007@gmail.com>
2019-04-25 01:58:41 +0900

    예제 수정

--- src/main/oop/interfaces/diamond_problem/classes/인어.java
+++ src/main/oop/interfaces/diamond_problem/classes/인어.java
@@ -4,6 +4,6 @@ package main.oop.interfaces.diamond_problem.classes;
 public class 인어 implements 사람, 물고기{
     @Override
     public void 헤엄치기() {
-        사람.super.헤엄치기();
+        사람.super.헤엄치기();//다중 상속 시 명확한 메소드 호출을 위해 상위 인터페이스의 메소드 명시
     }
 }
